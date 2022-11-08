bce60e4e61f0b77aa5489a02ed7fc3e79eae1cb7
MyungJiwoo <1206jiwoo@gmail.com>
2022-11-08 11:53:51 +0900

    기출 5: 세계맛기행축제

--- youngjin/Q5(세계맛기행)/js/script.js
+++ youngjin/Q5(세계맛기행)/js/script.js
@@ -1,1 +1,27 @@
-jQuery(document).ready(function () {});
+jQuery(document).ready(function () {
+  $(".navi>li")
+    .mouseover(function () {
+      $(this).find(".submenu").stop().slideDown(500);
+    })
+    .mouseout(function () {
+      $(this).find(".submenu").stop().slideUp(500);
+    });
+
+  // 좌-우 슬라이드
+  setInterval(function () {
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -800 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -1600 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: 0 });
+    $(".slidelist").delay(2000);
+  }, 3000);
+
+  $(".notice li:first").click(function () {
+    $("#modal").addClass("active");
+  });
+  $(".btn").click(function () {
+    $("#modal").removeClass("active");
+  });
+});
