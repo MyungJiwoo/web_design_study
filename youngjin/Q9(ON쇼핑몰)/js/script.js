bb921fb2368df14ce33b683b919c3a1269b5b6a0
MyungJiwoo <1206jiwoo@gmail.com>
2022-11-09 20:51:14 +0900

    기출 9: on 쇼핑몰 (self)

--- youngjin/Q9(ON쇼핑몰)/js/script.js
+++ youngjin/Q9(ON쇼핑몰)/js/script.js
@@ -1,6 +1,35 @@
 jQuery(document).ready(function () {
   // 메뉴
-  // Fade in, Fade out
+  $(".navi>li")
+    .mouseover(function () {
+      $(this).find(".submenu").stop().slideDown(500);
+    })
+    .mouseout(function () {
+      $(this).find(".submenu").stop().slideUp(500);
+    });
+
+  // Fade in, Fade out => 암기하기
+  $(".imgslide a:gt(0)").hide();
+  setInterval(function () {
+    $(".imgslide a:first")
+      .fadeOut()
+      .next("a")
+      .fadeIn()
+      .end()
+      .appendTo(".imgslide");
+  }, 3000);
+
   // tab
+  $(".tabmenu>li>a").click(function () {
+    $(this).parent().addClass("active").siblings().removeClass("active");
+    return false;
+  });
+
   // layer popup
+  $(".notice>ul>li>a:first").click(function () {
+    $("#layer").addClass("active");
+  });
+  $(".btn").click(function () {
+    $("#layer").removeClass("active");
+  });
 });
