ac4fc51f2ad32ffbfbb07c8ad93c8de7d2a84ae5
MyungJiwoo <1206jiwoo@gmail.com>
2022-11-11 01:04:23 +0900

    가로형 메뉴 연습

---
+++ study/menu(가로형)/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function () {
+  $(".navi>li")
+    .mouseover(function () {
+      $(".submenu").stop().slideDown(500);
+      $("#menu_bg").stop().slideDown(500);
+
+      //   $(this).find(".submenu").stop().slideDown(500);
+    })
+    .mouseout(function () {
+      $(".submenu").stop().slideUp(500);
+      $("#menu_bg").stop().slideUp(500);
+
+      //   $(this).find(".submenu").stop().slideUp(500);
+    });
+});
