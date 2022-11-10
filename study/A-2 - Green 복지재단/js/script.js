ab247c286c366d04c7bd83c35d60d22c6b62f328
MyungJiwoo <1206jiwoo@gmail.com>
2022-11-10 16:26:55 +0900

    green 복지재단 (menu 수정 전), self

---
+++ study/A-2 - Green 복지재단/js/script.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function () {
+  // 메뉴
+  $(".navi>li")
+    .mouseover(function () {
+      $(".submenu").stop().slideDown(500);
+    })
+    .mouseout(function () {
+      $(".submenu").stop().slideUp(500);
+    });
+
+  // 좌우 슬라이드
+  setInterval(function () {
+    $(".slidelist").animate({ marginLeft: "-1200px" });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: "-2400px" });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: "0px" });
+    $(".slidelist").delay(2000);
+  }, 3000);
+
+  // 모달
+  $(".notice>ul>li:first").click(function () {
+    $("#modal").addClass("active");
+  });
+  $(".btn").click(function () {
+    $("#modal").removeClass("active");
+  });
+});
