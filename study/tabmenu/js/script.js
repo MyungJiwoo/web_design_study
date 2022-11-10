f25be4e9ffec145f8b517bf840bcc146630f2f15
MyungJiwoo <1206jiwoo@gmail.com>
2022-11-10 11:49:33 +0900

    tabmenu

---
+++ study/tabmenu/js/script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+  $(function () {
+    $(".tabmenu>li>a").click(function () {
+      $(this).parent().addClass("active").siblings().removeClass("active");
+      return false;
+    });
+  });
+});
